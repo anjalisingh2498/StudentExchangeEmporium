6832af5e9e9f266042029a498a2a72a013a7abee
ANJALI SINGH <109875199+anjalisingh2498@users.noreply.github.com>
2024-09-15 21:13:33 +0530

    Update FavBook.jsx

--- frontend/src/home/FavBook.jsx
+++ frontend/src/home/FavBook.jsx
@@ -11,7 +11,7 @@ const FavBook = () => {
         </div>
 
         <div className='md:w-1/2 space-y-6'>
-            <h2 className='text-5xl font-bold my-5 md:w-3/4 leading-snug'>Find your Favorite<span className='text-blue-700'>Alumin Content Here</span></h2>
+            <h2 className='text-5xl font-bold my-5 md:w-3/4 leading-snug'>Find your Favorite<span className='text-blue-700'>Alumni Content Here</span></h2>
             <p className='mb-10 text-lg md:w-5/6'>Reconnect with your college network, share your experiences, and contribute to the next generation of students.</p>
 
             <div className='flex flex-col sm:flex-row justify-between gap-6 md:w-3/4 my-14'>
